8a1ed5081e664f767dba380506944e01b6b0232d
Edvin <edvin.thornholm@gmail.com>
2024-02-20 16:50:20 +0100

    half way

---
+++ WebApp/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.ViewModels;
+
+namespace WebApp.Controllers;
+
+public class AccountController : Controller
+{
+    //private readonly AccountService _accountService;
+
+    //public AccountController(AccountService accountService)
+    //{
+    //    _accountService = accountService;
+    //}
+
+    [Route("/account")]
+    public IActionResult Details()
+    {
+        var viewModel = new AccoundDetailsViewModel();
+        return View(viewModel);
+    }
+}
