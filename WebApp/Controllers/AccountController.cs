c1556de87c6ce502bbe1447105fedacf81d552c9
Edvin <edvin.thornholm@gmail.com>
2024-02-21 10:07:39 +0100

    account-details done

--- WebApp/Controllers/AccountController.cs
+++ WebApp/Controllers/AccountController.cs
@@ -16,6 +16,22 @@ public class AccountController : Controller
     public IActionResult Details()
     {
         var viewModel = new AccoundDetailsViewModel();
+        //viewModel.BasicInfo = _accountService.GetBasicInfo();
+        //viewModel.AddressInfo = _accountService.GetAddressInfo();
         return View(viewModel);
+    }
+
+    [HttpPost]
+    public IActionResult BasicInfo(AccoundDetailsViewModel viewModel)
+    {
+        //_accountService.SaveBasicInfo(viewModel.BasicInfo);
+        return RedirectToAction(nameof(Details));
+    }
+
+    [HttpPost]
+    public IActionResult AddressInfo(AccoundDetailsViewModel viewModel)
+    {
+        //_accountService.SaveAdressInfo(viewModel.AdressInfo);
+        return RedirectToAction(nameof(Details));
     }
 }
