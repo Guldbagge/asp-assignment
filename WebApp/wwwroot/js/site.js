335dccc79a355aae90cd5bc7fb0dc99c44db0906
Edvin <edvin.thornholm@gmail.com>
2024-03-08 09:09:49 +0100

    dark mode works

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -13,3 +13,29 @@
         console.error('Knappen eller mobilmenyn kunde inte hittas.');
     }
 });
+
+
+const switchMode = document.getElementById('switch-mode');
+const body = document.body;
+
+const isDarkMode = localStorage.getItem('darkMode') === 'true';
+
+switchMode.checked = isDarkMode;
+
+if (isDarkMode) {
+    body.classList.add('dark-mode');
+} else {
+    body.classList.remove('dark-mode');
+}
+
+switchMode.addEventListener('change', toggleDarkMode);
+
+function toggleDarkMode() {
+    if (switchMode.checked) {
+        body.classList.add('dark-mode');
+        localStorage.setItem('darkMode', 'true'); 
+    } else {
+        body.classList.remove('dark-mode');
+        localStorage.setItem('darkMode', 'false'); 
+    }
+}
