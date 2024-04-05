bfa139ba62289272849564b78e54c442eb6839b7
Edvin <edvin.thornholm@gmail.com>
2024-04-05 14:09:39 +0200

    Init

--- WebApp/Controllers/CoursesController.cs
+++ WebApp/Controllers/CoursesController.cs
@@ -23,23 +23,36 @@ public class CoursesController(CategoryService categoryService, CourseService co
     private readonly CategoryService _categoryService = categoryService;
     private readonly CourseService _courseService = courseService;
 
-    public async Task<IActionResult> Index(string category = "", string searchQuery ="", int pageNumber = 1, int pageSize = 6)
+    public async Task<IActionResult> Index(string category = "", string searchQuery = "", int pageNumber = 1, int pageSize = 6)
     {
-        var coursesResult = await _courseService.GetCoursesAsync(category, searchQuery, pageNumber, pageSize);
-        var viewModel = new CoursesIndexViewModel
+        try
         {
-            Categories = await _categoryService.GetCategoriesAsync(),
-            Courses = coursesResult.Courses,
-            Pagination = new Pagination
+            var coursesResult = await _courseService.GetCoursesAsync(category, searchQuery, pageNumber, pageSize);
+            var viewModel = new CoursesIndexViewModel
             {
-                PageSize = pageSize,
-                CurrentPage = pageNumber,
-                TotalPages = coursesResult.TotalPages,
-                TotalItems = coursesResult.TotlaItems
-            }
-        };
+                Categories = await _categoryService.GetCategoriesAsync(),
+                Courses = coursesResult.Courses,
+                Pagination = new Pagination
+                {
+                    PageSize = pageSize,
+                    CurrentPage = pageNumber,
+                    TotalPages = coursesResult.TotalPages,
+                    TotalItems = coursesResult.TotlaItems
+                }
+            };
 
-        return View(viewModel);
+            return View(viewModel);
+        }
+        catch (Exception)
+        {
+            ViewData["Status"] = "ConnectionFailed";
+
+            var viewModel = new CoursesIndexViewModel
+            {
+            };
+
+            return View(viewModel);
+        }
     }
 
     public async Task<IActionResult> Details(int id)
@@ -95,6 +108,36 @@ public class CoursesController(CategoryService categoryService, CourseService co
         return View();
     }
 
+
+
+    //public async Task<IActionResult> Index(string category = "", string searchQuery ="", int pageNumber = 1, int pageSize = 6)
+    //{
+    //    try
+    //    {
+    //        var coursesResult = await _courseService.GetCoursesAsync(category, searchQuery, pageNumber, pageSize);
+    //        var viewModel = new CoursesIndexViewModel
+    //        {
+    //            Categories = await _categoryService.GetCategoriesAsync(),
+    //            Courses = coursesResult.Courses,
+    //            Pagination = new Pagination
+    //            {
+    //                PageSize = pageSize,
+    //                CurrentPage = pageNumber,
+    //                TotalPages = coursesResult.TotalPages,
+    //                TotalItems = coursesResult.TotlaItems
+    //            }
+
+    //        };
+    //        return View(viewModel);
+    //    }
+
+    //    catch
+    //    {
+    //        ViewData["Status"] = "ConnectionFailed";
+    //    }
+
+    //    return View();
+    //}
 
     //[HttpPost]
     //public async Task<IActionResult> Create(CourseRegistrationFormViewModel model)
